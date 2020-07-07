7a4b617e9f08d2f88928dee1d2318ae3f9cbd852
Anand Raman <anandraman2020@u.northwestern.edu>
2020-07-07 09:31:04 -0500

    model update

--- lstm.py
+++ lstm.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 import torch.nn.functional as F
 import os
+from sklearn.metrics import roc_curve, auc
+import matplotlib as plt
+
 
 torch.manual_seed(308)
 
@@ -12,7 +15,7 @@ num_layers = 1
 hidden_size = 8
 learning_rate = 0.001
 batch_size = 128
-num_epochs = 2
+num_epochs = 100
 
 
 class LSTM(nn.Module):
@@ -43,15 +46,14 @@ class LSTM(nn.Module):
 
 # data goes to GPU
 device = torch.device('cuda')
-# avoiding some strange filepath issues related 
+# avoiding some strange filepath issues related
 # to gluster and kubernetes
-os.chdir("../")
 
 
-X_train = torch.load('home/X_train.pt')
-y_train = torch.load('home/y_train.pt')
-X_test = torch.load('home/X_test.pt')
-y_test = torch.load('home/y_test.pt')
+X_train = torch.load('data/X_train.pt')
+y_train = torch.load('data/y_train.pt')
+X_test = torch.load('data/X_test.pt')
+y_test = torch.load('data/y_test.pt')
 
 train_data = DataLoader(X_train, y_train)
 test_data = DataLoader(X_test, y_test)
@@ -65,6 +67,8 @@ lr = 0.001
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 loss = nn.CrossEntropyLoss()
 
+# output and label list
+prev_roc = 0
 for epoch in range(num_epochs):
     for batch_n, (X, y) in enumerate(train_data):
         X = X.to(device)
@@ -75,7 +79,43 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
         model.init_hidden(X)
         y_pred = model(X)
+        y_pred = torch.sigmoid(y_pred)
 
         batch_loss = loss(y_pred, y)
         batch_loss.backward()
         optimizer.step()
+
+    output_lst = []
+    label_lst = []
+
+    for batch_n, (X, y) in enumerate(train_data):
+        y_pred = model(X)
+        y_pred = torch.sigmoid(y_pred)
+        output_lst.append(y_pred.data)
+        label_lst.append(y)
+
+    output_lst = torch.cat(output_lst)
+    label_lst = torch.cat(label_lst)
+    pred_class = (y_pred > 0.5).float()
+    acc = torch.mean((pred_class == label_lst).float()).item() * 100
+
+    fpr, tpr, _ = roc_curve(y, y_pred)
+    roc_auc = auc(fpr, tpr)
+    if roc_auc > prev_roc:
+        prev_roc = roc_auc
+        torch.save(model.state_dict(), 'best_model.pt')
+        plt.figure()
+        lw = 2
+        plt.plot(fpr, tpr, color='dark orange', lw=lw,
+                 label='ROC Curve (area = %0.2f)' % roc_auc[2])
+        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic example')
+        plt.legend(loc="lower right")
+        plt.save('best_model_roc.png')
+
+    print('Epoch {0} Accuracy: {1}'.format(epoch, acc))
+    print('AUROC {}'.format(roc_auc[2]))
